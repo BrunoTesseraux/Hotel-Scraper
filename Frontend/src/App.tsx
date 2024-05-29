643eb2a82144788f69908630aad768b596772fb7
Bruno <bruno.tesseraux@gmx.de>
2024-05-29 18:22:02 +0200

    frontend übung und gedanken ins readme

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import HotelSelector from './assets/components/HotelSelector/HotelSelector';
 import SignIn from './assets/components/SignIn/SignIn';
 import SignUp from './assets/components/SignIn/SignUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -6,12 +7,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
+      {/* Weil ich die möglichkeit immer vergesse die h1 derweil */}
       <div>
         <h1>Hotel Scraper</h1>
         <Routes>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={<SignIn />} /> {/* Default route */}
+          <Route path="/" element={<HotelSelector />} /> {/* Default route */}
+
         </Routes>
       </div>
     </Router>
