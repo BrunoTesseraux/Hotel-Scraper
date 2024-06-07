46e38e313cd117a9f00bcb2945078ab6662b5fe8
Nikolas Sretkovic <145345708+NikolaSretko@users.noreply.github.com>
2024-06-07 18:52:20 +0200

    loggt die daten richtig nur das sichern in die db klappt noch nicht

--- backend/src/service/scraper/getIncludio.ts
+++ backend/src/service/scraper/getIncludio.ts
@@ -29,24 +29,25 @@ export const getIncludio = {
                 const element = priceElement as HTMLElement;
                 let inseratText = element ? element.innerText : 'Preis nicht gefunden';
                 return {
-                    inserat: inseratText
+                    inseratText
                 };
             });
         });
-        console.log(result);
 
         await browser.close();
 
         // Verarbeite die gescrapten Daten
-        const processedPrices = result.map(item => processScrapedData(item.inserat));
-
+        const processedPrices = result.map(item => processScrapedData(item.inseratText));
+        console.log('processedPrices: ', processedPrices);
+        
         // Erstelle ein Hotel-Dokument mit den übergebenen Hotel-Details und den gescrapten Preisen
         const hotelData = {
             ...hotelDetails,
             pricePerNight: processedPrices,
             pricePerNightFuture: []
         };
-
+        console.log('hotelData: ', hotelData);
+        
         const hotel = new HotelModel(hotelData);
         await hotel.save();
         return hotel;
@@ -55,17 +56,23 @@ export const getIncludio = {
 
 // Hilfsfunktion zur Verarbeitung der gescrapten Daten
 const processScrapedData = (inseratText: string): ScrapedPrice => {
-    const parts = inseratText.split('\n\n');
-    const typ = parts[0] || 'Typ nicht gefunden';
-    const size = parts[1] || 'Größe nicht gefunden';
-    const priceMatch = parts[2]?.match(/ab (\d+,\d+ €)/);
-    const price = priceMatch ? priceMatch[1] : 'Preis nicht gefunden';
+    // Regex Muster zur Extraktion von Typ, Größe und Preis
+    const regex = /(DOPPELZIMMER\s+[A-Z\s+\-]+|FAMILIENZIMMER)\s*\n*\s*(\d+\s*m²)\s*\n*\s*ab\s*(\d+,\d{2})\s*€/;
+
+    const parts = regex.exec(inseratText);
+
+    // Wenn die Regex keinen passenden Text findet, werden entsprechende Standardwerte gesetzt
+    const typ = parts ? parts[1].trim() : 'Typ nicht gefunden';
+    const size = parts ? parts[2].trim() : 'Größe nicht gefunden';
+    const price = parts ? parts[3].trim() : 'Preis nicht gefunden';
     const date = new Date(); // Setze das Datum entsprechend deiner Anforderungen
 
+    // console.log('Typ: ', typ, 'Größe: ', size, 'Preis: ', price);
+
     return {
-        typ,
-        size,
-        price,
-        date
+    typ,
+    size,
+    price,
+    date
     };
 };
